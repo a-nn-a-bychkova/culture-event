eb824b27150cbb9d1426df45a4732e54d184fd9d
Anna Bychkova <bychkova.dev@gmail.com>
2026-06-03 16:46:17 +0200

    concerdata

--- src/concertData.js
+++ src/concertData.js
@@ -4,24 +4,6 @@ export const ConcertsList = {
   de: [
     {
       id: uuidv4(),
-      date: 'So, 31. Mai 2026',
-      time: '14:30',
-      concertTitle: 'Konzert',
-      adressTitle:
-        'Pfarrheim der ukrainischen Ukrainische Pfarrei St.Nikolaus Nürnberg',
-      address: 'Eichstätter Platz 3, 90453 Nürnberg',
-      addressLink: 'https://maps.app.goo.gl/L86FWBDzmi4UAY7Y6',
-      ticket: '',
-      dauer: '20 Min.',
-      eventLink: '',
-      participants:
-        'Anna Bychkova (Mezzosopran), Nataliia Hrabarska (Mezzosopran, Bandura), Demian Matushevskyi (Bassbariton), Olena Vasylenko (Klavier)',
-      details:
-        'Ein kleines Konzert zu einem besonderen Anlass in der ukrainischen Ukrainische Pfarrei St.Nikolaus Nürnberg, das im Anschluss an das Pontifikalamt und den Moleben geleitet von Bischof Bohdan Dzyurakh stattfindet.',
-      eventbrite: '',
-    },
-    {
-      id: uuidv4(),
       date: 'So, 7. Juni 2026',
       time: '17:00',
       concertTitle: 'Gebet für die Ukraine',
@@ -32,7 +14,7 @@ export const ConcertsList = {
       dauer: '2 St.',
       eventLink: '',
       participants:
-        'Taras Konoshchenko (Bass, der Solist des Staatstheaters Nürnberg), Tetiana Korobka (Sopran, ausgezeichnete Künstlerin der Ukraine) und die Schüler ihres Gesangsstudios, Tetiana Manoilenko (Sopran), Daria Hrabovska (Sopran), Anna Bychkova (Mezzosopran), Nataliia Hrabarska (Mezzosopran, Bandura), Pedro Arroyo (Tenor), Yevhen Petronelli (Bariton), Anna Rydkina (Violine), Vadym Koviakh (Gitarre), Olena Vasylenko (Klavier), Mykhailo Grabovskyi (Percussion); Mariia Krasnokutska (Moderatorin)',
+        'Tetiana Korobka (Sopran, ausgezeichnete Künstlerin der Ukraine) und die Schüler ihres Gesangsstudios, Tetiana Manoilenko (Sopran), Daria Hrabovska (Sopran), Anna Bychkova (Mezzosopran), Nataliia Hrabarska (Mezzosopran, Bandura), Yevhen Petronelli (Bariton), Anna Rydkina (Violine), Andrii Dudar (Gitarre), Olena Vasylenko (Klavier), Mykhailo Grabovskyi (Percussion); Mariia Krasnokutska (Moderatorin)',
       details:
         'Kostbare Perlen der ukrainischen Musik — von dramatischen Opernarien bis zu herzergreifenden Romanzen',
       eventbrite: '',
@@ -51,29 +33,12 @@ export const ConcertsList = {
       participants:
         'Anna Bychkova (Mezzosopran), Nataliia Hrabarska (Mezzosopran, Bandura), Sofia Petishkina (Geige), Vadym Koviakh (Gitarre), Tetiana Manoilenko (Sopran), Svitlana Mykhailenko (Moderatorin), Olena Vasylenko (Klavier) ',
       details:
-        'Ukrainische Musik unter freiem Himmel. Diesen Abend widmen wir dem Komponisten Wolodymyr Iwasiuk. Ein stimmungsvolles Programm mit klassischen Stimmen, dem Kinderchor, den Bandura, der Violine, der Gitarre, dem Klavier im Innenhof des beliebten Kulturladen Zeltnerschloss erwartet Sie. Die Gäste haben die Möglichkeit, eigene Fragen an die Schwester des Komponisten Dr. Oksana Iwasiuk zu stellen. Das Projekt "Ukrainischer Lebensmut" fördert mit dem Erlös der Veranstaltung Hilfsprojekte in der Ukraine.',
+        'Ukrainische Musik unter freiem Himmel. Diesen Abend widmen wir dem Komponisten Wolodymyr Iwasiuk. Ein stimmungsvolles Programm mit klassischen Stimmen, dem Kinderchor, den Bandura, der Violine, der Gitarre, dem Klavier im Innenhof des beliebten Kulturladen Zeltnerschloss erwartet Sie. Die Gäste haben die Möglichkeit, eigene Fragen an die Schwester des Komponisten Dr. Oksana Iwasiuk zu stellen. Das Projekt "Ukrainischer Lebensmut" fördert mit dem Erlös der Veranstaltung Hilfsprojekte in der Ukraine. Koopperation mit Kulturladen Zeltnerschloß',
       eventbrite:
         'https://www.eventbrite.com/e/blume-der-hoffnung-tickets-1981141263851?aff=oddtdtcreator',
     },
   ],
   ukr: [
-    {
-      id: uuidv4(),
-      date: 'Нд, 31 травня 2026',
-      time: '14:30',
-      concertTitle: 'Концерт',
-      adressTitle: 'Домівка Pfarrei St.Nikolaus Nürnberg',
-      address: 'Eichstätter Platz 3, 90453 Nürnberg',
-      addressLink: 'https://maps.app.goo.gl/L86FWBDzmi4UAY7Y6',
-      ticket: '',
-      dauer: '20 хв',
-      eventLink: '',
-      participants:
-        "Анна Бичкова (мецо-сопрано), Наталія Грабарська (мецо-сопрано, бандура), Дем'ян Матушевський(бас-баритон), Олена Василенко (фортепіано)",
-      details:
-        'Невеличкий концерт з особливого приводу, відбудеться після архиєрейської божественної літургії, очолену єпископом Богданом Дзюрахом',
-      eventbrite: '',
-    },
     {
       id: uuidv4(),
       date: 'Нд, 7 червня 2026',
@@ -86,7 +51,7 @@ export const ConcertsList = {
       dauer: '2 год.',
       eventLink: '',
       participants:
-        "Тарас Конощенко (бас, соліст Державної опери Нюрнберга), Тетяна Коробка (сопрано, Заслужена артистка України) та учни її студії, Педро Арройо (тенор), Анна Бичкова (мецо-сопрано),  Євген Петронеллі (баритон), Тетяна Манойленко (сопрано), Дарія Грабовська (сопрано), Наталія Грабарська (мецо-сопрано, бандура), Анна Ридкіна (скрипка), Вадим Ков'ях (гітара), Олена Василенко (фортепіано), Михайло Грабовський (перкусія); ведуча – Марія Краснокутська",
+        'Тетяна Коробка (сопрано, Заслужена артистка України) та учни її студії,  Анна Бичкова (мецо-сопрано), Олена Василенко (фортепіано), Наталія Грабарська (мецо-сопрано, бандура), Дарія Грабовська (сопрано), Михайло Грабовський (перкусія), Андрій Дудар (гітара) Тетяна Манойленко (сопрано), Євген Петронеллі (баритон), Анна Ридкіна (скрипка); ведуча – Марія Краснокутська',
       details:
         'Перлини української музики - від драматичних оперних арій до вишуканих романсів',
       eventbrite: '',
@@ -105,7 +70,7 @@ export const ConcertsList = {
       participants:
         "Анна Бичкова(мецо-сопрано), Наталія Грабарська (мецо-сопрано, бандура),Тетяна Манойленко (сопрано), Світлана Михаленко (ведуча), Софія Петішкіна (скрипка), Вадим Ков'ях (гітара), Олена Василенко (фортепіано)",
       details:
-        'Українська музика просто неба. Цей вечір присвячено композитору Володимиру Івасюку. На вас чекає натхненна програма з класичними голосами, дитячим хором, бандурою, скрипкою, гітарою та фортепіано у внутрішньому дворі популярного культурного центру Kulturladen Zeltnerschloss. Гості матимуть можливість поставити свої запитання спеціальній гості - сестрі композитора — Оксані Івасюк. Проєкт «Ukrainischer Lebensmut» спрямовує виручені кошти від заходу на підтримку благодійних ініціатив в Україні.',
+        'Українська музика просто неба. Цей вечір присвячено композитору Володимиру Івасюку. На вас чекає натхненна програма з класичними голосами, дитячим хором, бандурою, скрипкою, гітарою та фортепіано у внутрішньому дворі популярного культурного центру Kulturladen Zeltnerschloss. Гості матимуть можливість поставити свої запитання спеціальній гості - сестрі композитора — Оксані Івасюк. Проєкт «Ukrainischer Lebensmut» спрямовує виручені кошти від заходу на підтримку благодійних ініціатив в Україні. Спільний проєкт з Kulturladen Zeltnerschloß',
       eventbrite:
         'https://www.eventbrite.com/e/blume-der-hoffnung-tickets-1981141263851?aff=oddtdtcreator',
     },
