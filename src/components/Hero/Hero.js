45883b20f69c351319f96cff38437f5ba93a50f6
Anna Bychkova <bychkova.dev@gmail.com>
2026-06-21 13:30:38 +0200

    poster

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import Poster from '../../images/dach-april-oben.jpg';
+import Poster from '../../images/Ein-Abend-Ivasyuk.png';
 import Context from '../../contexts/context';
 import { useContext } from 'react';
 import { Box } from '@mui/material';
